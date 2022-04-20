0cfedbdff387772e73e24c7913ebe59621d09129
rubylo718 <rubylo718@gmail.com>
2022-04-20 22:08:17 +0800

    feat: add mongoose connection

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 import express from "express"
+import mongoose from "mongoose"
+
 const app = express()
 const port = 3000
+
+mongoose.connect(process.env.MONGODB_URI_shortURL)
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error > <')
+})
+db.once('open', () => {
+  console.log('mongodb good good connected!')
+})
+
 
 app.get('/', (req, res)=> {
   res.send(`shortURL initial`)
